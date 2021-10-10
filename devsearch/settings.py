99d054d083695db73cc0dcfc5f89c49c107d58a7
ah_med <ahmed15-898@diu.edu.bd>
2021-10-11 00:41:19 +0600

    static file fixed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -231,8 +231,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env.str("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': env.str("CLOUDINARY_API_KEY"),
